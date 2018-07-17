21df07c30dc80f41c405fc846b4981bb4077c605
3014zhangshuo <3014zhangshuo@gmail.com>
2018-07-17 18:19:21 +0800

    support send image file

--- file.ts
+++ file.ts
@@ -1,11 +1,17 @@
+import * as path from 'path'
 import * as qrcodeTerminal  from 'qrcode-terminal'
 import { config } from './config'
-import { Wechaty, Contact, MediaMessage, FriendRequest } from 'wechaty'
+import { Wechaty, Contact, MediaMessage } from 'wechaty'
 
 // wondercv.wechaty.json store session
 const bot = Wechaty.instance({profile: 'wondercv'})
 const axios = require('axios')
 const qiniu = require('qiniu')
+
+const WONDERCV_PUB_IMAGE_FILE = path.join(
+  __dirname,
+  './file/wondercv-official-qr-code.jpg',
+)
 
 qiniu.conf.ACCESS_KEY = config.qiniu_access_key
 qiniu.conf.SECRET_KEY = config.qiniu_secret_key
@@ -32,10 +38,7 @@ bot
   try {
     logMsg = 'received `friend` event from ' + contact.get('name')
     fileHelper.say(logMsg)
-    console.log(logMsg)
-    let fr = new wechaty.FriendRequest()
-    fr.send(contact, 'hello')
-    contact.say('hello')
+
     if (request) {
       request.accept()
       contact.say('hello')
@@ -49,6 +52,15 @@ bot
 })
 .on('message', async message => {
   console.log(`RECV: ${message}`)
+
+  if (!message.self()) {
+    if (/电脑/.test(message)) {
+      await message.say("超级简历数据云存储，电脑手机可同步编辑！\n\n网址：wondercv.com\n或百度搜索 超级简历")
+    } else {
+      await message.say("目前仅支持中文简历导入，文件应为 pdf 或 doc 格式，直接发送给我即可上传\n如有其它问题请添加公众号咨询~")
+      await message.say(new MediaMessage(WONDERCV_PUB_IMAGE_FILE))
+    }
+  }
 
   if (message instanceof MediaMessage) {
     saveMediaFile(message)
@@ -66,7 +78,7 @@ async function saveMediaFile(message: MediaMessage) {
 
   if ((/(.txt|.doc|.docx|.word|.pdf)$/).test(filename)) {
     try {
-      await message.say('上传文件中..')
+      await message.say('正在上传文件… 请稍等')
       const netStream = await message.readyStream()
       const file_path = `wechaty_upload/${timestamp}/${user}/${filename}`
       const r_qiniu = await uploadToQiniu(netStream, file_path)
@@ -74,7 +86,7 @@ async function saveMediaFile(message: MediaMessage) {
         await message.say(`qiniu response file path ${r_qiniu}`)
       }
       const re = await axios.post(config.upload_file_url, {path: r_qiniu})
-      await message.say(`点击 ${re.data.path}`)
+      await message.say(`点击 ${re.data.path} \n 登录并读取简历，电脑手机可同步编辑`)
     } catch (e) {
       message.say(`error:${e}`)
       console.error('stream error:', e)
