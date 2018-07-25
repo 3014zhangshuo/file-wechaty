c98c06c019be6bbcea0f283504d05c342b2e6a69
3014zhangshuo <3014zhangshuo@gmail.com>
2018-07-25 14:36:29 +0800

    add sleep method

--- file.ts
+++ file.ts
@@ -2,10 +2,12 @@ import * as path from 'path'
 import * as qrcodeTerminal  from 'qrcode-terminal'
 import { config } from './config'
 import { Wechaty, Contact, MediaMessage } from 'wechaty'
+//import { DelayQueue } from 'rx-queue'
 // wondercv.wechaty.json store session
 const bot = Wechaty.instance({profile: 'wondercv'})
 const axios = require('axios')
 const qiniu = require('qiniu')
+//const delay = new DelayQueue(500)
 
 const WONDERCV_PUB_IMAGE_FILE = path.join(
   __dirname,
@@ -14,6 +16,8 @@ const WONDERCV_PUB_IMAGE_FILE = path.join(
 
 qiniu.conf.ACCESS_KEY = config.qiniu_access_key
 qiniu.conf.SECRET_KEY = config.qiniu_secret_key
+
+//delay.subscribe(bot)
 
 bot
 .on('scan', (url, code) => {
@@ -62,6 +66,7 @@ bot
         await message.say("超级简历数据云存储，电脑手机可同步编辑！\n\n网址：wondercv.com\n或百度搜索 超级简历")
       } else {
         await message.say("目前仅支持中文简历导入，文件应为 pdf 或 doc 格式，直接发送给我即可上传\n如有其它问题请添加公众号咨询")
+        await sleep(2000)
         await message.say(new MediaMessage(WONDERCV_PUB_IMAGE_FILE))
       }
     }
@@ -96,6 +101,7 @@ async function saveMediaFile(message: MediaMessage) {
         await message.say(`qiniu response file path ${r_qiniu}`)
       }
       const re = await axios.post(config.upload_file_url, {path: r_qiniu})
+      await sleep(1000)
       await message.say(`点击 ${re.data.path} \n登录并读取简历，电脑手机可同步编辑`)
     } catch (e) {
       message.say(`error:${e}`)
@@ -153,4 +159,8 @@ function fileSize(steam) {
 
 function bytesToKB(bytes) {
   return Math.round(bytes / 1024, 2)
+}
+
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms))
 }
